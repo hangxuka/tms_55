60d70f67bad49963817c799ebe84c5e4bd0d24f0
Hang Xuka <phamhangmta94@gmail.com>
2016-05-12 16:29:25 +0700

    code behind login-logout

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source "https://rubygems.org"
 
 gem "rails", "4.2.6"
 gem "bootstrap-sass", "3.2.0.0"
+gem "bootstrap-will_paginate", "0.0.10"
 gem "sqlite3"
 gem "sass-rails", "~> 5.0"
 gem "uglifier", ">= 1.3.0"
@@ -9,6 +10,7 @@ gem "coffee-rails", "~> 4.1.0"
 gem "jquery-rails"
 gem "turbolinks"
 gem "jbuilder", "~> 2.0"
+gem "bcrypt", "~> 3.1.7"
 gem "sdoc", "~> 0.4.0", group: :doc
 
 group :development, :test do
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,21 @@ class User < ActiveRecord::Base
   has_many :user_tasks, dependent: :destroy
   has_many :subjects, through: :user_subjects
   has_many :tasks, through: :user_tasks
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :email, presence: true, length: {maximum: 255},
+    format: {with: VALID_EMAIL_REGEX},
+    uniqueness: {case_sensitive: false}
+  validates :name, presence: true, length: {maximum: 50}
+  validates :password, presence: true, length: {minimum: 6}
+
+  before_save :email_downcase
+
+  has_secure_password
+  
+  private
+  def email_downcase
+    self.email = email.downcase
+  end
 end
