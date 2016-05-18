486f05a9382a4466cc35fb2ac14f23f5c08428bb
Hang Xuka <phamhangmta94@gmail.com>
2016-05-18 16:04:18 +0700

    show detail subject

--- app/controllers/supervisor/subjects_controller.rb
+++ app/controllers/supervisor/subjects_controller.rb
@@ -1,5 +1,15 @@
 class Supervisor::SubjectsController < ApplicationController
+  before_action :find_subject, only: [:show]
+
   def index
     @subjects = Subject.paginate page: params[:page]
+  end
+
+  def show
+  end
+
+  private
+  def find_subject
+    @subject = Subject.find params[:id]
   end
 end
