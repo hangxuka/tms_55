15f4da1f235492007859b46e1993956713a1314a
Hang Xuka <phamhangmta94@gmail.com>
2016-05-19 16:32:40 +0700

    create subject and task

--- app/controllers/supervisor/subjects_controller.rb
+++ app/controllers/supervisor/subjects_controller.rb
@@ -8,8 +8,26 @@ class Supervisor::SubjectsController < ApplicationController
   def show
   end
 
+  def new
+    @subject=Subject.new
+  end
+
+  def create
+    @subject = Subject.new subject_params
+    if @subject.save
+      flash[:success] = t "activerecord.controllers.supervisor.subjects.create_subject"
+      redirect_to supervisor_subject_path @subject
+    else
+      render :new
+    end
+  end
+
   private
   def find_subject
     @subject = Subject.find params[:id]
+  end
+
+  def subject_params
+    params.require(:subject).permit :name, :start_date, :end_date
   end
 end
--- app/models/subject.rb
+++ app/models/subject.rb
@@ -4,4 +4,15 @@ class Subject < ActiveRecord::Base
   has_many :tasks, dependent: :destroy
   has_many :user_subjects, dependent: :destroy
   has_many :users, through: :user_subjects
+
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validate  :end_date_greate_or_equal_than_start_date
+
+  private
+  def end_date_greate_or_equal_than_start_date
+    if start_date > end_date
+      errors.add :start_date, I18n.t("aplication.errors.subject.start_end")
+  end
+  end
 end
